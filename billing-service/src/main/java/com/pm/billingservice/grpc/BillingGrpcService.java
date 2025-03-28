4ef4aa924aa81d4d60550f1495206366b23dab66
Aswin Roy <aswinroy@yahoo.com>
2025-03-27 22:05:06 -0400

    set up grpc service

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,27 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@Slf4j
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<billing.BillingResponse> responseObserver) {
+
+        log.info("Create billing account request received{}", billingRequest.toString());
+
+        //Business logic - example save to database, perform calculations etc
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted(); 
+    }
+}
